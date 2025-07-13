7ace78ac55336b38c47e859017d36264cbb2e3b8
Anil <marisarlaanil08@gmail.com>
2025-07-13 23:28:34 +0530

    Added swagger plus cors issue

--- src/main/java/com/demoGcp/DemoProject/controller/DemoController.java
+++ src/main/java/com/demoGcp/DemoProject/controller/DemoController.java
@@ -1,10 +1,15 @@
 package com.demoGcp.DemoProject.controller;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@OpenAPIDefinition(servers = {
+        @Server(url = "https://spring-gcp-cloud-run-demo-706121786956.europe-west1.run.app")
+})
 public class DemoController {
 
     @Value("${myMessage}")
